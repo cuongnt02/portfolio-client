98c677b6cc3f114ad21c488e04b79a65929f8f63
cuongnt02 <cuong.ntc02@gmail.com>
2025-09-14 18:08:44 +0700

    [1409] CI - Change test scripts and add new one for CI mode

---
+++ vitest.browser.ci.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from "vitest/config";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  plugins: [react()],
+  test: {
+    browser: {
+      enabled: true,
+      provider: "webdriverio",
+      // https://vitest.dev/guide/browser/webdriverio
+      instances: [{ browser: "firefox", headless: true }],
+    },
+  },
+});
